4c0f0fad73fad389f888c47d75a694b3303a4b6b
Ubergamz <ubergamz@gmail.com>
2022-12-25 17:23:14 -0500

    1

--- RotaryPD.cs
+++ RotaryPD.cs
@@ -9,29 +9,23 @@ namespace _RotaryPD
     public class RotaryPD : Mastercam.App.NetHook3App
     {
         public Mastercam.App.Types.MCamReturn RotaryPDRun(Mastercam.App.Types.MCamReturn notused){
-            void ChangeOperations(){
-                var rotaryPD = 0.0;
-                DialogManager.AskForNumber("Enter New Rotary PD", ref rotaryPD);
-                OperationsManager.RefreshOperationsManager(true);
-                var operationIDs = SearchManager.GetOperationIDs(true);
-                var axisParams = new RotaryAxisParams
-                {
-                    Enabled = true,
-                    Diameter = rotaryPD,
-                    Type = RotaryAxisPositionType.AxisSubstitution,
-                    Direction = RotaryAxisDirection.CW,
-                    AxisSubstitution = RotaryAxisSubstitution.SubsituteY
-                };
-
-                foreach (var opID in operationIDs){
-                    var op = SearchManager.GetOperation(opID);
-                    op.RotaryAxis = axisParams;
-                    op.MarkDirty();
-                    op.Commit(false);
-                }
-                OperationsManager.RefreshOperationsManager(true);
+            OperationsManager.UnSelectAllOperations();
+            var surfaceFinishContourOpID = Mastercam.IO.Interop.SelectionManager.GetOperations(15);
+            var surfaceFinishPencilOpID = Mastercam.IO.Interop.SelectionManager.GetOperations(39);
+            foreach (var op in surfaceFinishContourOpID)
+            {
+                var thisOp = SearchManager.GetOperation(op);
+                thisOp.SetSelectedState(true);
+                Mastercam.IO.Interop.SelectionManager.AlterContourFinish(op, 0.2, -0.2, -0.2);
             }
-            ChangeOperations();
+            OperationsManager.RefreshOperationsManager(true);
+            foreach (var op in surfaceFinishPencilOpID)
+            {
+                var thisOp = SearchManager.GetOperation(op);
+                thisOp.SetSelectedState(true);
+            }
+            OperationsManager.RefreshOperationsManager(true);
+
             return MCamReturn.NoErrors;
         }
     }
