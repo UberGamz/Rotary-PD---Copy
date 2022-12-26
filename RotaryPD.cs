7d3c8a9fc50b94a536c500425c3fde59e0358288
Ubergamz <ubergamz@gmail.com>
2022-12-26 13:19:48 -0500

    Complete

--- RotaryPD.cs
+++ RotaryPD.cs
@@ -3,26 +3,55 @@ using Mastercam.App.Types;
 using Mastercam.Support;
 using Mastercam.Operations.Types;
 using Mastercam.Operations;
+using System.Collections.Generic;
+using Mastercam.Database;
 
 namespace _RotaryPD
 {
     public class RotaryPD : Mastercam.App.NetHook3App
     {
         public Mastercam.App.Types.MCamReturn RotaryPDRun(Mastercam.App.Types.MCamReturn notused){
-            OperationsManager.UnSelectAllOperations();
+            var contourList = new List<int>();
+            var pencilList = new List<int>();
+            var selectedOps = SearchManager.GetOperations(true);
             var surfaceFinishContourOpID = Mastercam.IO.Interop.SelectionManager.GetOperations(15);
             var surfaceFinishPencilOpID = Mastercam.IO.Interop.SelectionManager.GetOperations(39);
             foreach (var op in surfaceFinishContourOpID)
             {
-                var thisOp = SearchManager.GetOperation(op);
-                thisOp.SetSelectedState(true);
-                Mastercam.IO.Interop.SelectionManager.AlterContourFinish(op, 0.2, -0.2, -0.2);
+                foreach (var thisOp in selectedOps)
+                {
+                    var selectedOp = thisOp.GetOperationID();
+                    if (selectedOp == op)
+                    {
+                        contourList.Add(selectedOp);
+                    }
+                }
             }
-            OperationsManager.RefreshOperationsManager(true);
             foreach (var op in surfaceFinishPencilOpID)
             {
+                foreach (var thisOp in selectedOps)
+                {
+                    var selectedOp = thisOp.GetOperationID();
+                    if (selectedOp == op)
+                    {
+                        pencilList.Add(selectedOp);
+                    }
+                }
+            }
+            foreach (var op in contourList)
+            {
+                Mastercam.IO.Interop.SelectionManager.AlterContourFinish(0.2, -0.2, -0.2);
                 var thisOp = SearchManager.GetOperation(op);
-                thisOp.SetSelectedState(true);
+                thisOp.MarkDirty();
+                thisOp.Commit(false);
+            }
+            OperationsManager.RefreshOperationsManager(true);
+            foreach (var op in pencilList)
+            {
+                Mastercam.IO.Interop.SelectionManager.AlterPencilFinish(true, -0.020, 0.00);
+                var thisOp = SearchManager.GetOperation(op);
+                thisOp.MarkDirty();
+                thisOp.Commit(false);
             }
             OperationsManager.RefreshOperationsManager(true);
 
